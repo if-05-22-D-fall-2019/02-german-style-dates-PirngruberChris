20b89c6b2ed33c48ae986825dcbc7b1e706f7fea
PirngruberChris <christianpirngruber3@gmail.com>
2020-06-07 13:25:38 +0200

    all tests run

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        String regex = "^(3(1|0)|[0-2]?\\d)(\\.|\\-|\\s)(0?\\d|1[0-2](\\.|\\-|\\s)(19|20)?\\d\\d$";
+        String regex = "^(3(1|0)|[0-2]?\\d)(\\.|\\-|\\s)(0?\\d|1[0-2])(\\.|\\-|\\s)(19|20)?\\d\\d$";
         Patter pattern = pattern.compile(regex);
         Matcher matcher = pattern.matcher(dateString);
 
