e0ad6de6587cc7937f13a579463f4cc6457d4b1e
PirngruberChris <christianpirngruber3@gmail.com>
2020-06-07 13:23:49 +0200

    some test run

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,12 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        String regex = "^(3(1|0)|[0-2]?\\d)(\\.|\\-|\\s)(0?\\d|1[0-2](\\.|\\-|\\s)(19|20)?\\d\\d$";
+        Patter pattern = pattern.compile(regex);
+        Matcher matcher = pattern.matcher(dateString);
+
+
+
+        return matcher;
     }
 }
